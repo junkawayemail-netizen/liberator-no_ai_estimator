e17868ef01a36942be4c71f665c4e07ce1828d7f
daniam <junkaway.email@gmail.com>
2025-12-03 02:19:57 -0800

    Add files via upload

--- index.tsx
+++ index.tsx
@@ -1,4 +1,3 @@
-
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
@@ -9,6 +8,7 @@ if (!rootElement) {
 }
 
 const root = ReactDOM.createRoot(rootElement);
+
 root.render(
   <React.StrictMode>
     <App />
